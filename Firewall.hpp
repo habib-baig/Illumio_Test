5e0853777d0208f967ff999046cd4dee2639d8b3
habib-baig <mhhabib7@gmail.com>
2018-11-11 22:01:32 -0600

    4th oush

--- Firewall.hpp
+++ Firewall.hpp
@@ -19,7 +19,7 @@ class Firewall{
     Firewall(string path);
     void push_rule(string &s);
     bool accept_packet(string dir, string protocol, int port, string ipaddress);
-    bool search(vector<rule> &rules, int port, string ipaddress);
+    bool linearsearch(vector<rule> &rules, int port, string ipaddress);
     bool binarySearch(vector<rule> &rules, int port, string ipaddress);
     void printrules(vector<rule> &rules);
     void printallowedrules();
